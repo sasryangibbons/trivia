8afeb72d785680c20a1a5c82f0177c518fe59b37
rygibb <ryan.gibbons@sas.com>
2023-02-03 17:15:40 -0500

    trying to figure out routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import App from './App';
 import store from './store/index';
@@ -13,9 +13,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
+
 
     </Provider>
   </React.StrictMode>
